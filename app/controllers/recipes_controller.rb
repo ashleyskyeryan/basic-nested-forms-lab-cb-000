2144ab97c910e3d5c00727d219748c7e0b5f2db2
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-14 21:04:35 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,9 +10,11 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
     @recipe.ingredients.build(name: "--First Ingredient--")
-    @recipe.ingredients.build(name: "Second Ingredient--")
+    @recipe.ingredients.build(name: "--Second Ingredient--")
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
   end
 end
