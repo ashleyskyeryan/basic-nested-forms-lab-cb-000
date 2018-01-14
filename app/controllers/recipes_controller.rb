430f386815feda6e73ee19b16d0bc99839f7af07
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-14 21:05:17 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,4 +17,8 @@ class RecipesController < ApplicationController
     @recipe = Recipe.create(recipe_params)
     redirect_to recipe_path(@recipe)
   end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
 end
